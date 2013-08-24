ae3447094eb129c9e8bd204bf3b6ede7e94b39e2
Toms Bergmanis <toms.bergmanis@gmail.com>
2013-08-25 00:38:22 +0100

    Added Knight Class

--- piece.py
+++ piece.py
@@ -3,6 +3,10 @@ DOWN = -1
 RIGHT = 1
 LEFT = -1
 
+BMAX = 9
+BMIN = 0
+
+
 class ChessPiece(object):
 
 	def __init__(self, x, y):
@@ -33,11 +37,11 @@ class Pawn(object):
 			moves.append((self.piece.pos_x,self.piece.pos_y + 2*dir))
 		
 		# take piece on the right
-		if self.piece.pos_x + 1 < 9:
+		if self.piece.pos_x + 1 <  BMAX:
 			moves.append((self.piece.pos_x + 1, self.piece.pos_y + 1*dir))
 			
 		# take piece on the left
-		if self.piece.pos_x - 1 < 0:
+		if self.piece.pos_x - 1 < BMIN:
 			moves.append((self.piece.pos_x - 1, self.piece.pos_y + 1*dir))
 			
 		return moves
@@ -50,10 +54,10 @@ class Rook(object):
 
 	def move_options(self):
 		moves = []
-		for i in range(1,9):
+		for i in range(1,BMAX):
 			moves.append((self.piece.pos_x, i))
 			
-		for i in range(1,9):
+		for i in range(1, BMAX):
 			moves.append((i, self.piece.pos_y))
 			
 		return moves
@@ -75,14 +79,54 @@ class Bishop(object):
 
 		return moves
 		
-	def __extend_diognal__(self,x_dir, y_dir):
+	def __extend_diognal__(self, x_dir, y_dir):
 		squares = []
 		x = self.piece.pos_x + x_dir
 		y = self.piece.pos_y + y_dir
 		
-		while x < 9 and y < 9 and x > 0 and y > 0:
+		while x <  BMAX and y < BMAX and x > BMIN and y > BMIN:
 			squares.append((x,y))
 			x = x + x_dir
 			y = y + y_dir
 
 		return squares
+		
+
+class Knight(object):
+
+	def __init__(self, piece):
+		self.piece = piece
+		
+	def move_options(self):
+		moves = []
+		x = self.piece.pos_x
+		y = self.piece.pos_y
+		if y + 2 < BMAX:
+			if x - 1 > BMIN:
+				moves.append((x - 1, y + 2))
+			
+			if x + 1 < BMAX:
+				moves.append((x + 1, y + 2))
+
+		if y - 2 > BMIN:
+			if x - 1 > BMIN:
+				moves.append((x - 1, y - 2))
+			
+			if x + 1 < BMAX:
+				moves.append((x + 1, y - 2))
+				
+		if x + 2 < BMAX:
+			if y - 1 > BMIN:
+				moves.append((x + 2, y - 1))
+			
+			if y + 1 < BMAX:
+				moves.append((x + 2, y + 1))
+
+		if x - 2 > BMIN:
+			if y - 1 > BMIN:
+				moves.append((x - 2, y - 1))
+			
+			if y + 1 < BMAX:
+				moves.append((x - 2, y + 1))
+
+		return moves
